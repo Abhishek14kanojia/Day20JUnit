85a4a70cbf45f9e1b6b468c7ab1da3bd6774925f
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2023-01-14 21:08:24 +0530

    UC1

---
+++ mainuserRegistrationTest.java
@@ -0,0 +1,21 @@
+package com.day20Junit;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class mainuserRegistrationTest {
+	UserRegistration user = new UserRegistration();
+	@Test
+	public void givenFirstName_true() {
+		boolean actual = user.firstName("Abhishek");
+		boolean expected = true;
+		Assertions.assertEquals(expected, actual);
+	}
+	@Test
+	public void givenFirstName_false() {
+		boolean actual = user.firstName("abhishek");
+		boolean expected = false;
+		Assertions.assertEquals(expected, actual);
+	}
+	
+}
